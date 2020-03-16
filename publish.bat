b69e78965660834e3a7e5449e99647418241184a
scott krause <neodigm@users.noreply.github.com>
2020-03-16 09:20:53 -0500

    Delete publish.bat

--- publish.bat
+++
@@ -1,6 +0,0 @@
-git config --global user.name "neodigm"
-git config --global user.email "neodigm@users.noreply.github.com"
-git status .
-git stage .
-git commit -m content
-git push origin gh-pages
