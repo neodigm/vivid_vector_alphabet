7933239c01a0977f64dc089c950d6af6d8b48ff8
arcanus55 <NicholasKrause@users.noreply.sudo>
2020-04-09 14:55:04 -0500

    sfdc link

---
+++ regarding_scott_c_krause.js
@@ -0,0 +1,25 @@
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "address": {
+        "@type": "PostalAddress",
+        "addressLocality": "Lake County",
+        "addressRegion": "IL",
+        "postalCode": "60031"
+      },
+      "email": "neo5ive@gmail.com",
+      "image": "http://www.thescottkrause.com/ux/typescript/engineer/wasm/self.png",
+      "jobTitle": "Sr. UX Front-end Engineer",
+      "name": "Scott Krause",
+      "url": "https://www.theScottKrause.com",
+	    "sameAs" : [ "https://github.com/neodigm",
+	    "https://www.linkedin.com/in/neodigm24/",
+	    "https://trailblazer.me/id/skrause",
+	    "https://codepen.io/neodigm24",
+	    "https://machvive.com/",
+	    "https://www.arcanus55.com/",
+      "https://repl.it/@neodigm",
+      "https://twitter.com/neodigm24"]
+    }
+</script>
