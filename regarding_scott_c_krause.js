494f57b6874238e8f81c409a65a499c58239edbe
Scott C. Krause <3151842+neodigm@users.noreply.github.com>
2020-05-22 10:09:13 -0500

    🚀JavaScript && TypeScript && Go 🪐

--- regarding_scott_c_krause.js
+++ regarding_scott_c_krause.js
@@ -1,27 +1,28 @@
 <script type="application/ld+json">
-    {
-      "@context": "https://schema.org",
-      "@type": "Person",
-      "address": {
-        "@type": "PostalAddress",
-        "addressLocality": "Lake County",
-        "addressRegion": "IL",
-        "postalCode": "60031"
-      },
-      "email": "neo5ive@gmail.com",
-      "image": "http://www.thescottkrause.com/ux/typescript/engineer/wasm/self.png",
-      "jobTitle": "Sr. UX Front-end Engineer",
-      "name": "Scott Krause",
-      "url": "https://www.theScottKrause.com",
-	    "sameAs" : [ "https://github.com/neodigm",
-	    "https://www.theScottKrause.com/",
-		"https://www.npmjs.com/~neodigm",
-	    "https://www.linkedin.com/in/neodigm24/",
-	    "https://trailblazer.me/id/skrause",
-	    "https://codepen.io/neodigm24",
-	    "https://machvive.com/",
-	    "https://www.arcanus55.com/",
-      "https://repl.it/@neodigm",
-      "https://twitter.com/neodigm24"]
-    }
+  {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "Lake County",
+      "addressRegion": "IL",
+      "postalCode": "60031"
+    },
+    "email": "neo5ive@gmail.com",
+    "image": "https://neodigm.github.io/brand_logo_graphic_design/fantastic/discerning/24.jpg",
+    "jobTitle": "Sr. UX Front-end Engineer",
+    "name": "Scott Krause",
+    "url": "https://www.theScottKrause.com",
+    "sameAs" : [ "https://github.com/neodigm",
+    "https://www.theScottKrause.com/",
+  "https://gist.github.com/neodigm",
+  "https://www.npmjs.com/~neodigm",
+    "https://www.linkedin.com/in/neodigm24/",
+    "https://trailblazer.me/id/skrause",
+    "https://codepen.io/neodigm24",
+    "https://machvive.com/",
+    "https://www.arcanus55.com/",
+    "https://repl.it/@neodigm",
+    "https://twitter.com/neodigm24"]
+  }
 </script>
